08447864e604310e62dba93f1dfba2e0b6aa0ba8
Sri Krishna Sathwik <67147860+sathwik-s@users.noreply.github.com>
2021-08-31 15:05:00 +0530

    Create scipy.py

---
+++ scipy.py
@@ -0,0 +1,8 @@
+  
+#scipy
+import numpy as np
+ from scipy import io as sio
+ array = np.ones((4, 4))
+ sio.savemat('example.mat', {'ar': array}) 
+ data = sio.loadmat(‘example.mat', struct_as_record=True)
+ data['ar']
